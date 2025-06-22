e2b39e5825db56d3a870ed069729d3d8bb87ebe6
hkc619 <kendrick10112001@gmail.com>
2025-06-22 23:30:03 +0800

    Main func done. readme need to be finished. 0622, 2025

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import { Command } from "commander";
 import { userAct } from "./services/userAct.js";
+import chalk from "chalk";
 import "dotenv/config";
 
 const program = new Command();
@@ -10,16 +11,24 @@ const program = new Command();
 program
   .name("github-activity-tool")
   .description(
-    "Use CLI to connect GitHub API to fetch user activity and display it in the terminal. "
+    "Use CLI to connect GitHub API to fetch user activity and display it in the terminal."
   )
-  .version("0.0.1");
+  .version("0.1.0");
 
 // Main func
 program
   .argument("<userName>", "Username of user's activities you want to check.")
+  .option("--page <number>", "The page number of the results to fetch.", 1)
+  .option(
+    "--per-page <number>",
+    "The number of results per page (max 100).",
+    10
+  )
   .action((userName) => {
-    console.log(userName);
-    userAct(userName);
+    const options = program.opts();
+    //console.log(options.page);
+    console.log(chalk.blue(userName));
+    userAct(userName, options.perPage, options.page);
   });
 
 program.parse();
